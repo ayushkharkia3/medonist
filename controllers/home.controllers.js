b1df13643ff8cb5188b717534ae9130c45dac6af
ayushkharkia3 <ayushkharkia3@gmail.com>
2020-08-13 18:03:33 +0530

    .

--- controllers/home.controllers.js
+++ controllers/home.controllers.js
@@ -1,3 +1,22 @@
-exports.getIndex = (req, res, next) => {
-    res.render('index');
+const moment = require('moment');
+const Reminder = require('../models/Reminders');
+
+exports.getIndex = async(req, res, next) => {
+    let med = [];
+    let time = 7;
+    if (Number(moment().format('HH')) >= 9 && Number(moment().format('HH')) < 14) {
+        time = 12;
+        med = await Reminder.find({ userID: req.session.user._id, afternoon: true }).sort({ beforeFood: -1 })
+    } else if (Number(moment().format('HH')) >= 14 && Number(moment().format('HH')) < 19) {
+        time = 17;
+        med = await Reminder.find({ userID: req.session.user._id, night: true }).sort({ beforeFood: -1 })
+    } else if (Number(moment().format('HH')) >= 21) {
+        time = 7;
+        med = await Reminder.find({ userID: req.session.user._id, morning: true }).sort({ beforeFood: -1 });
+    } else {
+        time = 7;
+        med = await Reminder.find({ userID: req.session.user._id, morning: true }).sort({ beforeFood: -1 })
+    }
+
+    res.render('index', { med, time });
 }
